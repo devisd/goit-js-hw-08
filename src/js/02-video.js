a389d13b572949c25028392bfd98f958bec75b17
devisd <89788799+devisd@users.noreply.github.com>
2022-03-21 22:02:54 +0700

    Task 2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,23 @@
+// импорт throttle из библиотеки lodash (установлен только один NPM пакет, а не вся библиотека)
+import throttle from "lodash.throttle";
+
+// инициализация плеера Vimeo
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+// колбэк-фукция записи значания времени воспроизведения в локальное хранилище
+const onPlay = function(data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
 
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+// запуск плеера с отложенным колбэком в 1 сек.
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+// сохранение значения из локального хранилища и преобразование этого значения в объект
+const savedData = localStorage.getItem('videoplayer-current-time');
+const parsetData = JSON.parse(savedData);
+
+// установка воспроизведения видео из распарсеного значения локального хранилища
+player.setCurrentTime(parsetData.seconds);
+
